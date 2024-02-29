8a3d028e9e7fea71ad8e287f8d4923ea5fd6a90a
bozidarsk <bozidarkabahcijski@gmail.com>
2024-02-29 18:54:17 +0200

    Added more interrupt flags.

--- Kernel/Interrupts/InterruptFlags.cs
+++ Kernel/Interrupts/InterruptFlags.cs
@@ -2,6 +2,11 @@ namespace Kernel.Interrupts;
 
 public enum InterruptFlags : byte
 {
-	Interrupt = 0x0e,
-	Present = 0x80,
+	Interrupt = 0b_0000_1110,
+	Trap      = 0b_0000_1111,
+	Present   = 0b_1000_0000,
+	RING0     = 0b_0000_0000,
+	RING1     = 0b_0010_0000,
+	RING2     = 0b_0100_0000,
+	RING3     = 0b_0110_0000,
 }
